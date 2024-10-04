896b39d91fae55cc26b2d027d1eea0ff530182ff
Animesh <iamanimesh02@gmail.com>
2024-10-04 21:06:15 +0530

    testing remote origin

--- UserService/src/main/java/com/micro/user/service/external/services/HotelServices.java
+++ UserService/src/main/java/com/micro/user/service/external/services/HotelServices.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.List;
+
 @Service
 @FeignClient(name = "HOTEL-SERVICE")
 public interface HotelServices {
